04518331abd6968a82de7f8dd7e6d9e1887e50d7
kase2228 <tizitabelachew80@gmail.com>
2024-11-18 13:45:25 +0300

    readme

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,4 +1,15 @@
-def index_range(page, page_size):
+#!/usr/bin/env python3
+"""
+    prints the start and end index of the provided
+    page arguments
+"""
+def index_range(page: int, page_size:int) -> tuple:
+    """"
+        accepts page number and size
+        returns tuple of size two: 
+            Start index and
+            end index
+    """
     start_index = (page -1) * page_size
     end_index = start_index + page_size
     A = (start_index, end_index)
