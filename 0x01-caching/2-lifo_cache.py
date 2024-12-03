4b8b110b348ec32404b0503d82c8be1c7c693ca9
kase2228 <tizitabelachew80@gmail.com>
2024-12-03 12:14:49 +0300

    task2

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" LIFO Cache module """
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """
+    inherits from BaseCaching and 
+    perform LIFO caching Mechanism
+    """
+
+    def __init__(self):
+        """
+        initialize LIFOCache
+        """
+
+        super().__init__()
+        self.last_key = None
+
+    def put(self, key ,item):
+        """
+        add an item in LIFOCahing mechanism
+        """
+
+        if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
+                print(f"DISCARD: {self.last_key}")
+                del self.cache_data[self.last_key]
+
+            self.cache_data[key] = item
+            self.last_key = key
+
+    def get(self, key):
+        """
+        return an item using the given key
+        """
+
+        return self.cache_data.get(key, None)
