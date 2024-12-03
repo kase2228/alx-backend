49fb8ccab651fd224b85fbb8fc625279402265d5
kase2228 <tizitabelachew80@gmail.com>
2024-12-03 13:31:49 +0300

    task 4

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+""" MRU Cache module """
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+    inherits from BaseCaching and performs
+    MRU caching mechanism
+    """
+
+    def __init__(self):
+        """
+        initialize the MRUCache
+        """
+        super().__init__()
+        self.most_recent = None
+
+    def put(self, key, item):
+        """
+        add an item to the MRUCache
+        """
+
+        if key is not None and item is not None:
+            if key in self.cache_data:
+                self.cache_data[key] = item
+            else:
+                if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                    print(f"DISCARD: {self.most_recent}")
+                    del self.cache_data[self.most_recent]
+
+                self.most_recent = key
+    
+    def get(self, key):
+        """
+        get an item from the cache using key
+        if key exists marked as most recently used
+        """
+
+        if key in self.cache_data:
+            self.most_recent = key
+            return self.cache_data[key]
+        
+        return None
