0c80279676b78359b6dd0ec2f7601fd6da3df7ad
kase2228 <tizitabelachew80@gmail.com>
2024-12-03 12:16:16 +0300

    task2

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -23,7 +23,8 @@ class LIFOCache(BaseCaching):
         """
 
         if key is not None and item is not None:
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
+            if len(self.cache_data
+                   ) >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
                 print(f"DISCARD: {self.last_key}")
                 del self.cache_data[self.last_key]
 
